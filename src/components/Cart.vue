486457d7d8f4dd5e85e7c3dddb841598153c22ff
David Freitas <davidfreitas.dev@outlook.com>
2022-02-03 19:07:23 -0300

    Cart done

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,5 +1,193 @@
 <template>
-  <div></div>
+  <main>
+		<section class="page-banner bg-secondary">
+			<div class="wrapper page-banner-desc">
+				<h1>Shopping Cart</h1>
+				<div><span class="text-main">Home</span> / <span>cart</span></div>
+			</div>
+		</section>
+
+		<div class="product-cart">
+			<div class="wrapper">
+				<div class="cart-collection">
+					<div class="cart-header">
+						<p>Item</p>
+						<p>Quantity</p>
+						<p>Unit Price</p>
+						<p>Total</p>
+					</div>
+					<form action="">
+						<div class="cart-item">						
+							<div class="cart-product">
+								<div class="cart-image" style="background-image:url(img/intel.png)"></div>
+								<div class="cart-product-info">
+									<p class="cart-product-name">Intel Core i5 9th Gen</p>
+									<p class="cart-price-sm">$1.250</p>
+									<small>x 1</small>
+								</div>
+							</div>
+							<div class="cart-quantity-md">
+								<div class="cart-quantity-controls">
+									<button>-</button>
+									<input type="number" value="1" readonly />
+									<button>+</button>
+								</div>
+							</div>
+							<div class="cart-unit-price">
+								<h4>$1.250</h4>
+							</div>
+							<div class="cart-product-total">
+								<h4>$1.250</h4>
+							</div>
+							<div class="cart-controls-sm">
+								<div class="remove">
+									<span class="ti-trash"></span> Remove
+								</div>
+								<div class="cart-quantity-controls-sm">
+									<button>-</button>
+									<input type="number" value="1" readonly />
+									<button>+</button>
+								</div>
+							</div>
+						</div>
+
+						<div class="cart-item">						
+							<div class="cart-product">
+								<div class="cart-image" style="background-image:url(img/amd.png)"></div>
+								<div class="cart-product-info">
+									<p class="cart-product-name">AMD Ryzen 5 2400G</p>
+									<p class="cart-price-sm">$1.250</p>
+									<small>x 1</small>
+								</div>
+							</div>
+							<div class="cart-quantity-md">
+								<div class="cart-quantity-controls">
+									<button>-</button>
+									<input type="number" value="1" readonly />
+									<button>+</button>
+								</div>
+							</div>
+							<div class="cart-unit-price">
+								<h4>$1.250</h4>
+							</div>
+							<div class="cart-product-total">
+								<h4>$1.250</h4>
+							</div>
+							<div class="cart-controls-sm">
+								<div class="remove">
+									<span class="ti-trash"></span> Remove
+								</div>
+								<div class="cart-quantity-controls-sm">
+									<button>-</button>
+									<input type="number" value="1" readonly />
+									<button>+</button>
+								</div>
+							</div>
+						</div>
+
+						<div class="cart-item">						
+							<div class="cart-product">
+								<div class="cart-image" style="background-image:url(img/ssd-nvme.png)"></div>
+								<div class="cart-product-info">
+									<p class="cart-product-name">SSD NVME Corsair</p>
+									<p class="cart-price-sm">$1.250</p>
+									<small>x 1</small>
+								</div>
+							</div>
+							<div class="cart-quantity-md">
+								<div class="cart-quantity-controls">
+									<button>-</button>
+									<input type="number" value="1" readonly />
+									<button>+</button>
+								</div>
+							</div>
+							<div class="cart-unit-price">
+								<h4>$1.250</h4>
+							</div>
+							<div class="cart-product-total">
+								<h4>$1.250</h4>
+							</div>
+							<div class="cart-controls-sm">
+								<div class="remove">
+									<span class="ti-trash"></span> Remove
+								</div>
+								<div class="cart-quantity-controls-sm">
+									<button>-</button>
+									<input type="number" value="1" readonly />
+									<button>+</button>
+								</div>
+							</div>
+						</div>
+
+						<div class="cart-item">						
+							<div class="cart-product">
+								<div class="cart-image" style="background-image:url(img/kitgamer.png)"></div>
+								<div class="cart-product-info">
+									<p class="cart-product-name">Kit Mouse, Teclado e Headset Gamer</p>
+									<p class="cart-price-sm">$1.250</p>
+									<small>x 1</small>
+								</div>
+							</div>
+							<div class="cart-quantity-md">
+								<div class="cart-quantity-controls">
+									<button>-</button>
+									<input type="number" value="1" readonly />
+									<button>+</button>
+								</div>
+							</div>
+							<div class="cart-unit-price">
+								<h4>$1.250</h4>
+							</div>
+							<div class="cart-product-total">
+								<h4>$1.250</h4>
+							</div>
+							<div class="cart-controls-sm">
+								<div class="remove">
+									<span class="ti-trash"></span> Remove
+								</div>
+								<div class="cart-quantity-controls-sm">
+									<button>-</button>
+									<input type="number" value="1" readonly />
+									<button>+</button>
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+				
+				<div class="cart-total-holder">
+					<div class="cart-total">
+						<p>Total:</p>
+						<p>$1.250</p>
+					</div>
+
+					<div class="cart-action-button">
+						<a href="index.html">Continue Shopping</a>
+						<a href="checkout.html" class="btn-main">Proceed to Checkout</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<section class="mobile-contact-section bg-secondary">
+			<div class="wrapper">
+				<div class="section-info">					
+					<h2>About</h2>
+
+					<div class="section-desc">
+						<span>Developed By <a href="https://davidfreitas.dev.br/">David Freitas</a></span>
+						<span>See on <a href="#">Github</a></span>
+					</div>
+					
+					<div class="mobile-social-links">
+						<span class="ti-linkedin"></span>
+						<span class="ti-github"></span>
+						<span class="ti-twitter-alt"></span>
+					</div>
+				</div>				
+			</div>
+		</section>
+	</main>
 </template>
 
 <script>
@@ -9,5 +197,216 @@ export default {
 </script>
 
 <style>
+  .product-cart {
+    margin-top: 1rem;
+  }
 
+  .cart-collection {
+    margin-top: 0rem;
+  }
+
+  .cart-item {
+    display: grid;
+    grid-template-columns: 100%;
+    grid-gap: 0rem 1.3rem;
+    border: 1px solid #efefef;
+    margin-bottom: 1rem;
+  }
+
+  .cart-product {
+    display: grid;
+    grid-template-columns: 80px auto;
+    grid-gap: 0rem 1.3rem;
+    padding: 1rem 1rem;
+  }
+
+  .cart-image {
+    height: 75px;
+    border-radius: 7px;
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+
+  .cart-quantity-md,
+  .cart-unit-price,
+  .cart-product-total {
+    display: none;
+  }
+
+  .cart-controls-sm {
+    display: flex;
+    justify-content: space-between;
+    padding: .8rem;
+  }
+
+  .remove {
+    font-weight: bold;
+    text-transform: uppercase;
+    color: #505050;
+  }
+
+  .cart-quantity-controls-sm {
+    display: flex;
+  }
+
+  .cart-quantity-controls-sm button,
+  .cart-quantity-controls-sm input,
+  .cart-quantity-controls button,
+  .cart-quantity-controls input {
+    height: 25px;
+    width: 25px;
+    border: none;
+    outline: none;
+    text-align: center;
+    font-weight: bold;
+    font-size: 1.1rem;
+    color: #505050;
+    margin-left: .3rem;
+  }
+
+  .cart-quantity-controls-sm button,
+  .cart-quantity-controls button {
+    background: var(--main);
+    color: #fff;
+    border-radius: 50%;
+  }
+
+  .cart-product-name {
+    font-weight: bold;
+    color: #505050;
+    margin-bottom: .3rem;
+  }
+
+  .cart-price-sm {
+    color: var(--main);
+    font-weight: bold;
+    margin-bottom: .3rem;
+  }
+
+  .cart-product-info small {
+    font-weight: bold;
+    color: #333;
+  }
+
+  .cart-total-holder {
+    margin-top: 1rem;
+  }
+
+  .cart-total {
+    margin-bottom: 1rem;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .cart-total p {
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+
+  .cart-action-button {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .cart-action-button a {
+    display: block;
+    text-align: center;
+    font-weight: 600;
+    padding: .7rem;
+    margin-bottom: .5rem;
+    border: 4px solid var(--dark);
+    border-radius: 5px;
+  }
+
+  .cart-action-button a:first-child {
+    order: 1;
+    background: var(--dark);
+    color: #fff;
+  }
+
+  .cart-header {
+    display: none;
+  }
+
+  @media only screen and (min-width: 768px) {
+    .cart-quantity-md,
+    .cart-unit-price,
+    .cart-product-total {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 15%;
+    }
+
+    .cart-item:first-child {
+      border-top: 1px solid #efefef;
+    }
+
+    .cart-item {
+      border: none;
+      display: flex;
+      border-bottom: 1px solid #efefef;
+    }
+
+    .cart-controls-sm {
+      display: none;
+    }
+
+    .cart-product {
+      border: none;
+      flex: 1;
+      align-items: center;
+    }
+
+    .cart-product small {
+      display: none;
+    }
+    
+    .product-cart {
+      margin-top: 0rem;
+      padding-bottom: 2rem;
+    }
+
+    .cart-collection {
+      margin-top: 1rem;
+    }
+
+    .cart-header {
+      display: flex;
+      padding: 1rem 0rem;
+    }
+
+    .cart-header p {
+      font-weight: bold;
+      color: #505050;
+      width: 16.5%;
+      text-align: center;
+    }
+
+    .cart-header p:first-child {
+      width: auto;
+      flex: 1;
+      text-align: left;
+    }
+
+    .cart-total,
+    .cart-action-button {
+      justify-content: flex-end;
+      flex-direction: row;
+    }
+
+    .cart-total p:first-child {
+      margin-right: 1rem;
+    }
+
+    .cart-action-button a {
+      padding-left: 1.5rem;
+      padding-right: 1.5rem;
+    }
+
+    .cart-action-button a:first-child {
+      margin-right: 1.2rem;
+      order: 0;
+    }
+  }
 </style>
