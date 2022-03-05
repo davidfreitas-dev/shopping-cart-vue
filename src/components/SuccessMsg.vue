04e70ffde2351f27e67a5d56ede655c6ebd9a903
David Freitas <davidfreitas@MacBook-Pro-de-David.local>
2022-03-05 20:12:43 -0300

    Success Message Component Finished

--- src/components/SuccessMsg.vue
+++ src/components/SuccessMsg.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="modal success-msg" v-if="true">
+    <div class="modal success-msg" v-if="visibility">
         <div class="modal-content">
             <span class="close" @click="hideSuccessMsg">&times;</span>
-            <p>Some text in the Modal..</p>
+            <h2>Congratulations!</h2>
+            <div class="img" :style="{ backgroundImage: 'url(' + require('@/assets/img/check.gif') + ')' }"></div>
+            <button>View Order</button>
         </div>
     </div>
 </template>
@@ -35,40 +37,64 @@ export default {
 <style scoped>
 /* The Modal (background) */
 .modal {
-  position: fixed; /* Stay in place */
-  z-index: 1; /* Sit on top */
-  left: 0;
-  top: 0;
-  width: 100%; /* Full width */
-  height: 100%; /* Full height */
-  overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
-  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+    position: fixed; /* Stay in place */
+    z-index: 1; /* Sit on top */
+    left: 0;
+    top: 0;
+    width: 100%; /* Full width */
+    height: 100%; /* Full height */
+    overflow: auto; /* Enable scroll if needed */
+    background-color: rgb(0,0,0); /* Fallback color */
+    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
 }
 
 /* Modal Content/Box */
 .modal-content {
-  background-color: #fefefe;
-  margin: 25% auto; /* 15% from the top and centered */
-  padding: 20px;
-  border: 1px solid #888;
-  border-radius: 7px;
-  width: 80%; /* Could be more or less, depending on screen size */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #fefefe;
+    margin: 25% auto; /* 15% from the top and centered */
+    padding: 20px;
+    border: 1px solid #888;
+    border-radius: 7px;
+    width: 80%; /* Could be more or less, depending on screen size */
 }
 
 /* The Close Button */
 .close {
-  color: #aaa;
-  float: right;
-  font-size: 28px;
-  font-weight: bold;
+    align-self: flex-end;
+    color: #aaa;
+    float: right;
+    font-size: 28px;
+    font-weight: bold;
+    margin-top: -10px;
 }
 
 .close:hover,
 .close:focus {
-  color: black;
-  text-decoration: none;
-  cursor: pointer;
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+}
+
+.img {
+    width: 150px;
+    height: 150px;
+    margin: 1rem 0;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+}
+
+button {
+    outline: none;
+    border: none;
+    color: #fff;
+    margin: 1rem 0;
+    padding: .5rem 2rem;
+    background-color: var(--main);
 }
 
 @media only screen and (min-width: 768px) {			
