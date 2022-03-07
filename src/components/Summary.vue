7de63fde4a23bba51cbb84e722db14ced7566157
David Freitas <davidfreitas@MacBook-Pro-de-David.local>
2022-03-07 20:36:12 -0300

    Checkout Page Design

---
+++ src/components/Summary.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="summary">
+        <div class="summary-items">
+            <span class="title">Summary</span>
+            <div class="item">
+              <p>Processor Intel</p>
+              <span>$300.00</span>
+            </div>
+            <div class="item">
+              <p>AMD Processor</p>
+              <span>$200.00</span>
+            </div>
+            <div class="item">
+              <p>Motherboard</p>
+              <span>$600.00</span>
+            </div>
+            <div class="item">
+              <p>SSD Corsair</p>
+              <span>$150.00</span>
+            </div>
+        </div>
+        <div class="summary-actions">
+            <button>Finish</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.summary {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: left;
+  margin: 1rem .5rem;
+  padding: 1.3rem;
+  border: 1px solid #c3c3c3;
+}
+
+.summary-items .title {
+  font-size: 1.5rem;
+  font-weight: 700;
+}
+
+.summary-items .item {
+  display: flex;
+  justify-content: space-between;
+  margin-top: .75rem;
+  margin-bottom: 0;
+  border-bottom: 1px solid #c3c3c3;
+}
+
+.item span {
+  font-size: 1.1rem;
+  font-weight: 600;
+}
+
+.summary-actions button {
+  outline: none;
+  border: none;
+  color: #fff;
+  font-size: 1.3rem;
+  font-weight: 600;
+  letter-spacing: .05rem;
+  text-transform: uppercase;
+  width: 100%;
+  padding: .85rem 0;
+  margin-top: 1rem;
+  background-color: var(--main);
+  cursor: pointer;
+}
+
+.summary-actions button:hover {
+  background-color: #eb6550;
+}
+
+@media only screen and (min-width: 768px) {
+  .summary {
+    width: 40%;
+    margin: 0 .5rem;
+  }
+
+  .summary-actions button {
+    margin-top: 0;
+  }
+}
+</style>
--- src/pages/Checkout.vue
+++ src/pages/Checkout.vue
@@ -1,26 +1,74 @@
 <template>
-  <div class="checkout">
-      <input type="text" name="zipcode" />
-      <input type="text" name="Address" />
-      <input type="text" name="number" />
-      <input type="text" name="complement" />
-      <input type="text" name="neighborhood" />
-      <input type="text" name="city" />
-      <input type="text" name="state" />
-      <input type="text" name="country" />
-  </div>
+  <main>
+    <div class="wrapper">
+      <div class="section-info">
+        <h3>Checkout</h3>
+        <hr class="separator">
+      </div>
+      <div class="checkout">
+        <div class="form-checkout">
+          <input type="text" name="zipcode" placeholder="zipcode"/>
+          <input type="text" name="address" placeholder="address"/>
+          <input type="text" name="number" placeholder="number"/>
+          <input type="text" name="complement" placeholder="complement"/>
+          <input type="text" name="neighborhood" placeholder="neighborhood"/>
+          <input type="text" name="city" placeholder="city"/>
+          <input type="text" name="state" placeholder="state"/>
+          <input type="text" name="country" placeholder="country"/>
+        </div>
+        <Summary />
+      </div>
+    </div>
+    <About />
+  </main>
 </template>
 
 <script>
-export default {
+import Summary from '../components/Summary.vue'
+import About from '../template/About.vue'
 
+export default {
+  components: {
+    About,
+    Summary
+  },
 }
 </script>
 
 <style>
 .checkout {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-bottom: 3rem;
+}
+
+.form-checkout {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 .5rem;
+}
+
+.form-checkout input {
+  color: var(--dark);
+  font-size: 1rem;
+  width: 100%;
+  padding: .85rem;
+  margin-bottom: .5rem;
+  border: 1px solid #c3c3c3;
+  border-radius: 5px;
+  outline: none;
+}
+
+@media only screen and (min-width: 768px) {
+  .checkout {
+    flex-direction: row;
+  }
+
+  .form-checkout {
+    width: 60%;
+  }
 }
 </style>
